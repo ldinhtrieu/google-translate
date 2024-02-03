a79deb321428832011dead69d38df320e8780634
ldinhtrieu <ldinhtrieu@gmail.com>
2024-02-03 17:15:15 +0700

    remove bing

--- data/options/index.js
+++ data/options/index.js
@@ -1,152 +1,136 @@
-'use strict';
+"use strict";
 
-const toast = document.getElementById('toast');
+const toast = document.getElementById("toast");
 
 function restore() {
-  chrome.storage.local.get({
-    'width': 400,
-    'mheight': 600,
-    'scale': 1.0,
-    'offset-x': 0,
-    'offset-y': 0,
-    'domain': 'com',
-    'use-pointer': true,
-    'force-inside': true,
-    'direct-frame': false,
-    'google-page': true,
-    'bing-page': false,
-    'reuse-page': true,
-    'default-action': 'open-google',
-    'faqs': true,
-    'translate-styles': '',
-    'hide-translator': true,
-    'google-extra': '',
-    'bing-extra': 'from=&to=fr'
-  }, prefs => {
-    document.getElementById('force-inside').checked = prefs['force-inside'];
-    document.getElementById('width').value = prefs.width;
-    document.getElementById('mheight').value = prefs.mheight;
-    document.getElementById('scale').value = prefs.scale;
-    document.getElementById('offset-x').value = prefs['offset-x'];
-    document.getElementById('offset-y').value = prefs['offset-y'];
-    document.getElementById('domain').value = prefs.domain;
-    document.getElementById('google-page').checked = prefs['google-page'];
-    document.getElementById('bing-page').checked = prefs['bing-page'];
-    document.getElementById('reuse-page').checked = prefs['reuse-page'];
-    document.getElementById('default-action').value = prefs['default-action'];
-    document.getElementById('faqs').checked = prefs.faqs;
-    document.getElementById('hide-translator').checked = prefs['hide-translator'];
-    document.getElementById('google-extra').value = prefs['google-extra'];
-    document.getElementById('bing-extra').value = prefs['bing-extra'];
-    if (prefs['use-pointer'] && prefs['direct-frame'] === false) {
-      document.getElementById('use-pointer').checked = true;
+  chrome.storage.local.get(
+    {
+      width: 400,
+      mheight: 600,
+      scale: 1.0,
+      "offset-x": 0,
+      "offset-y": 0,
+      domain: "com",
+      "use-pointer": true,
+      "force-inside": true,
+      "direct-frame": false,
+      "google-page": true,
+      "reuse-page": true,
+      "default-action": "open-google",
+      "translate-styles": "",
+      "hide-translator": true,
+      "google-extra": "",
+    },
+    (prefs) => {
+      document.getElementById("force-inside").checked = prefs["force-inside"];
+      document.getElementById("width").value = prefs.width;
+      document.getElementById("mheight").value = prefs.mheight;
+      document.getElementById("scale").value = prefs.scale;
+      document.getElementById("offset-x").value = prefs["offset-x"];
+      document.getElementById("offset-y").value = prefs["offset-y"];
+      document.getElementById("domain").value = prefs.domain;
+      document.getElementById("google-page").checked = prefs["google-page"];
+      document.getElementById("reuse-page").checked = prefs["reuse-page"];
+      document.getElementById("default-action").value = prefs["default-action"];
+      document.getElementById("hide-translator").checked =
+        prefs["hide-translator"];
+      document.getElementById("google-extra").value = prefs["google-extra"];
+      if (prefs["use-pointer"] && prefs["direct-frame"] === false) {
+        document.getElementById("use-pointer").checked = true;
+      } else if (prefs["direct-frame"]) {
+        document.getElementById("use-direct").checked = true;
+      } else {
+        document.getElementById("use-selection").checked = true;
+      }
+      document.getElementById("translate-styles").value =
+        prefs["translate-styles"];
     }
-    else if (prefs['direct-frame']) {
-      document.getElementById('use-direct').checked = true;
-    }
-    else {
-      document.getElementById('use-selection').checked = true;
-    }
-    document.getElementById('translate-styles').value = prefs['translate-styles'];
-  });
+  );
 }
 
 function save() {
   const prefs = {
-    'force-inside': document.getElementById('force-inside').checked,
-    'width': Math.min(Math.max(Number(document.getElementById('width').value), 300), 2000),
-    'mheight': Number(document.getElementById('mheight').value),
-    'scale': Math.min(Math.max(parseFloat(document.getElementById('scale').value), 0.5), 1.0),
-    'offset-x': Number(document.getElementById('offset-x').value),
-    'offset-y': Number(document.getElementById('offset-y').value),
-    'domain': document.getElementById('domain').value,
-    'use-pointer': document.getElementById('use-pointer').checked,
-    'direct-frame': document.getElementById('use-direct').checked,
-    'google-page': document.getElementById('google-page').checked,
-    'bing-page': document.getElementById('bing-page').checked,
-    'reuse-page': document.getElementById('reuse-page').checked,
-    'default-action': document.getElementById('default-action').value,
-    'faqs': document.getElementById('faqs').checked,
-    'translate-styles': document.getElementById('translate-styles').value,
-    'hide-translator': document.getElementById('hide-translator').checked,
-    'google-extra': document.getElementById('google-extra').value,
-    'bing-extra': document.getElementById('bing-extra').value
+    "force-inside": document.getElementById("force-inside").checked,
+    width: Math.min(
+      Math.max(Number(document.getElementById("width").value), 300),
+      2000
+    ),
+    mheight: Number(document.getElementById("mheight").value),
+    scale: Math.min(
+      Math.max(parseFloat(document.getElementById("scale").value), 0.5),
+      1.0
+    ),
+    "offset-x": Number(document.getElementById("offset-x").value),
+    "offset-y": Number(document.getElementById("offset-y").value),
+    domain: document.getElementById("domain").value,
+    "use-pointer": document.getElementById("use-pointer").checked,
+    "direct-frame": document.getElementById("use-direct").checked,
+    "google-page": document.getElementById("google-page").checked,
+    "reuse-page": document.getElementById("reuse-page").checked,
+    "default-action": document.getElementById("default-action").value,
+    "translate-styles": document.getElementById("translate-styles").value,
+    "hide-translator": document.getElementById("hide-translator").checked,
+    "google-extra": document.getElementById("google-extra").value,
   };
 
   chrome.storage.local.set(prefs, () => {
-    toast.textContent = 'Options saved.';
-    setTimeout(() => toast.textContent = '', 750);
+    toast.textContent = "Options saved.";
+    setTimeout(() => (toast.textContent = ""), 750);
     restore();
   });
 }
 
-document.addEventListener('DOMContentLoaded', restore);
-document.getElementById('save').addEventListener('click', () => {
+document.addEventListener("DOMContentLoaded", restore);
+document.getElementById("save").addEventListener("click", () => {
   try {
     save();
-  }
-  catch (e) {
+  } catch (e) {
     toast.textContent = e.message;
-    setTimeout(() => toast.textContent = '', 750);
+    setTimeout(() => (toast.textContent = ""), 750);
   }
 });
 
-chrome.storage.onChanged.addListener(prefs => {
-  const mouse = prefs['use-pointer'];
+chrome.storage.onChanged.addListener((prefs) => {
+  const mouse = prefs["use-pointer"];
   if (mouse) {
     if (mouse.newValue) {
-      chrome.contextMenus.remove('open-panel');
-    }
-    else {
+      chrome.contextMenus.remove("open-panel");
+    } else {
       chrome.contextMenus.create({
-        id: 'open-panel',
-        title: 'Translate Selection',
-        contexts: ['selection'],
-        documentUrlPatterns: ['*://*/*']
+        id: "open-panel",
+        title: "Translate Selection",
+        contexts: ["selection"],
+        documentUrlPatterns: ["*://*/*"],
       });
     }
   }
-  const google = prefs['google-page'];
+  const google = prefs["google-page"];
   if (google) {
     if (google.newValue) {
       chrome.contextMenus.create({
-        id: 'open-google',
-        title: 'Translate with Google',
-        contexts: ['page', 'link'],
-        documentUrlPatterns: ['*://*/*']
+        id: "open-google",
+        title: "Translate with Google",
+        contexts: ["page", "link"],
+        documentUrlPatterns: ["*://*/*"],
       });
-    }
-    else {
-      chrome.contextMenus.remove('open-google');
-    }
-  }
-  const bing = prefs['bing-page'];
-  if (bing) {
-    if (bing.newValue) {
-      chrome.contextMenus.create({
-        id: 'open-bing',
-        title: 'Translate with Bing',
-        contexts: ['page', 'link'],
-        documentUrlPatterns: ['*://*/*']
-      });
-    }
-    else {
-      chrome.contextMenus.remove('open-bing');
+    } else {
+      chrome.contextMenus.remove("open-google");
     }
   }
 });
 
 // support
-document.getElementById('support').addEventListener('click', () => chrome.tabs.create({
-  url: chrome.runtime.getManifest().homepage_url + '?rd=donate'
-}));
+document.getElementById("support").addEventListener("click", () =>
+  chrome.tabs.create({
+    url: chrome.runtime.getManifest().homepage_url + "?rd=donate",
+  })
+);
 // reset
-document.getElementById('reset').addEventListener('click', e => {
+document.getElementById("reset").addEventListener("click", (e) => {
   if (e.detail === 1) {
-    toast.textContent = 'Double-click to reset!';
-    window.setTimeout(() => toast.textContent = '', 750);
-  }
-  else {
+    toast.textContent = "Double-click to reset!";
+    window.setTimeout(() => (toast.textContent = ""), 750);
+  } else {
     localStorage.clear();
     chrome.storage.local.clear(() => {
       chrome.runtime.reload();
@@ -156,8 +140,8 @@ document.getElementById('reset').addEventListener('click', e => {
 });
 
 // links
-for (const a of [...document.querySelectorAll('[data-href]')]) {
-  if (a.hasAttribute('href') === false) {
-    a.href = chrome.runtime.getManifest().homepage_url + '#' + a.dataset.href;
+for (const a of [...document.querySelectorAll("[data-href]")]) {
+  if (a.hasAttribute("href") === false) {
+    a.href = chrome.runtime.getManifest().homepage_url + "#" + a.dataset.href;
   }
 }
--- worker.js
+++ worker.js
@@ -1,96 +1,107 @@
 /* global position */
-'use strict';
+"use strict";
 
-const open = (tab, query, frameId, permanent = false) => chrome.scripting.executeScript({
-  target: {
-    tabId: tab.id,
-    frameIds: [frameId]
-  },
-  func: () => ({
-    position: typeof position === 'undefined' ? null : position
-  })
-}, ([{result}]) => {
-  const position = {
-    sx: 0,
-    sy: 0
-  };
-  chrome.storage.local.get({
-    'width': 400,
-    'mheight': 600,
-    'translate-styles': '',
-    'scale': 1.0,
-    'force-inside': true,
-    'hide-translator': true,
-    'google-extra': '',
-    'domain': 'com'
-  }, prefs => {
-    chrome.windows.get(tab.windowId, async win => {
-      if (result.position) {
-        Object.assign(position, result.position);
-      }
-      else {
-        position.sx = win.left;
-        position.sy = win.top;
-      }
+const open = (tab, query, frameId, permanent = false) =>
+  chrome.scripting.executeScript(
+    {
+      target: {
+        tabId: tab.id,
+        frameIds: [frameId],
+      },
+      func: () => ({
+        position: typeof position === "undefined" ? null : position,
+      }),
+    },
+    ([{ result }]) => {
+      const position = {
+        sx: 0,
+        sy: 0,
+      };
+      chrome.storage.local.get(
+        {
+          width: 400,
+          mheight: 600,
+          "translate-styles": "",
+          scale: 1.0,
+          "force-inside": true,
+          "hide-translator": true,
+          "google-extra": "",
+          domain: "com",
+        },
+        (prefs) => {
+          chrome.windows.get(tab.windowId, async (win) => {
+            if (result.position) {
+              Object.assign(position, result.position);
+            } else {
+              position.sx = win.left;
+              position.sy = win.top;
+            }
 
-      // Avoid popup outside the screen
-      if (prefs['force-inside']) {
-        const {height, width, left, top} = await chrome.windows.getCurrent();
-        position.sy = Math.min(position.sy, top + height - prefs.mheight);
-        position.sx = Math.min(position.sx, left + width - prefs.width);
-      }
+            // Avoid popup outside the screen
+            if (prefs["force-inside"]) {
+              const { height, width, left, top } =
+                await chrome.windows.getCurrent();
+              position.sy = Math.min(position.sy, top + height - prefs.mheight);
+              position.sx = Math.min(position.sx, left + width - prefs.width);
+            }
 
-      const url = 'https://translate.google.' + prefs.domain + '/?' +
-        (prefs['google-extra'] ? prefs['google-extra'] + '&' : '') +
-        'text=' + encodeURIComponent(query);
+            const url =
+              "https://translate.google." +
+              prefs.domain +
+              "/?" +
+              (prefs["google-extra"] ? prefs["google-extra"] + "&" : "") +
+              "text=" +
+              encodeURIComponent(query);
 
-      chrome.windows.create({
-        url,
-        left: parseInt(position.sx),
-        top: parseInt(position.sy),
-        width: parseInt(prefs.width),
-        height: parseInt(prefs.mheight),
-        type: 'popup'
-      }).catch(e => {
-        console.warn(e);
+            chrome.windows
+              .create({
+                url,
+                left: parseInt(position.sx),
+                top: parseInt(position.sy),
+                width: parseInt(prefs.width),
+                height: parseInt(prefs.mheight),
+                type: "popup",
+              })
+              .catch((e) => {
+                console.warn(e);
 
-        return chrome.windows.create({
-          url,
-          width: parseInt(prefs.width),
-          height: parseInt(prefs.mheight),
-          type: 'popup'
-        });
-      }).then(w => {
-        prefs.permanent = permanent;
-        open.ids[w.tabs[0].id] = prefs;
-      });
-    });
-  });
-});
+                return chrome.windows.create({
+                  url,
+                  width: parseInt(prefs.width),
+                  height: parseInt(prefs.mheight),
+                  type: "popup",
+                });
+              })
+              .then((w) => {
+                prefs.permanent = permanent;
+                open.ids[w.tabs[0].id] = prefs;
+              });
+          });
+        }
+      );
+    }
+  );
 open.ids = {};
 
 const onMessage = (request, sender, response) => {
-  if (request.method === 'open-translator') {
+  if (request.method === "open-translator") {
     open(sender.tab, request.query, sender.frameId, request.permanent);
 
     chrome.scripting.executeScript({
       target: {
         tabId: sender.tab.id,
-        allFrames: true
+        allFrames: true,
       },
       func: () => {
         try {
           clearTimeout(self.pointer.rid);
           self.pointer.hide();
-        }
-        catch (e) {}
-      }
+        } catch (e) {}
+      },
     });
-  }
-  else if (request.method === 'close') {
+  } else if (request.method === "close") {
     chrome.tabs.remove(sender.tab.id);
-  }
-  else if (request.method === 'extend') {
+  } else if (request.method === "extend") {
     response(open.ids[sender.tab.id]);
     delete open.ids[sender.tab.id];
   }
@@ -98,109 +109,91 @@ const onMessage = (request, sender, response) => {
 chrome.runtime.onMessage.addListener(onMessage);
 
 const onClicked = (info, tab) => {
-  if (info.menuItemId === 'open-panel') {
+  if (info.menuItemId === "open-panel") {
     open(tab, info.selectionText, info.frameId);
-  }
-  else {
-    chrome.storage.local.get({
-      'domain': 'com',
-      'google-extra': '',
-      'bing-extra': 'from=&to=fr',
-      'reuse-page': true
-    }, prefs => {
-      let link = info.linkUrl || info.pageUrl;
-      if (link.startsWith('about:reader?url=')) {
-        link = decodeURIComponent(link.replace('about:reader?url=', ''));
+  } else {
+    chrome.storage.local.get(
+      {
+        domain: "com",
+        "google-extra": "",
+        "reuse-page": true,
+      },
+      (prefs) => {
+        let link = info.linkUrl || info.pageUrl;
+        if (link.startsWith("about:reader?url=")) {
+          link = decodeURIComponent(link.replace("about:reader?url=", ""));
+        }
+        let url =
+          `https://translate.google.${prefs.domain}/translate` +
+          `?${
+            prefs["google-extra"] ? prefs["google-extra"] + "&" : ""
+          }u=${encodeURIComponent(link)}`;
+
+        // when this is a page translation, offer redirection
+        if (!info.linkUrl && prefs["reuse-page"]) {
+          chrome.tabs.update(tab.id, { url });
+        } else {
+          chrome.tabs.create({
+            url,
+            index: tab.index + 1,
+          });
+        }
       }
-      let url = `https://translate.google.${prefs.domain}/translate` +
-        `?${prefs['google-extra'] ? prefs['google-extra'] + '&' : ''}u=${encodeURIComponent(link)}`;
-      if (info.menuItemId === 'open-bing') {
-        url = `http://www.microsofttranslator.com/bv.aspx?${prefs['bing-extra'] ? prefs['bing-extra'] + '&' : ''}` +
-          `a=${encodeURIComponent(link)}`;
-      }
-      // when this is a page translation, offer redirection
-      if (!info.linkUrl && prefs['reuse-page']) {
-        chrome.tabs.update(tab.id, {url});
-      }
-      else {
-        chrome.tabs.create({
-          url,
-          index: tab.index + 1
-        });
-      }
-    });
+    );
   }
 };
 
 /* context menu */
 {
   const onStartup = () => {
-    chrome.storage.local.get({
-      'use-pointer': true,
-      'google-page': true,
-      'bing-page': false
-    }, prefs => {
-      if (prefs['use-pointer'] === false) {
-        chrome.contextMenus.create({
-          id: 'open-panel',
-          title: 'Translate Selection',
-          contexts: ['selection'],
-          documentUrlPatterns: ['*://*/*']
-        }, () => chrome.runtime.lastError);
+    chrome.storage.local.get(
+      {
+        "use-pointer": true,
+        "google-page": true,
+      },
+      (prefs) => {
+        if (prefs["use-pointer"] === false) {
+          chrome.contextMenus.create(
+            {
+              id: "open-panel",
+              title: "Translate Selection",
+              contexts: ["selection"],
+              documentUrlPatterns: ["*://*/*"],
+            },
+            () => chrome.runtime.lastError
+          );
+        }
+        if (prefs["google-page"]) {
+          chrome.contextMenus.create(
+            {
+              id: "open-google",
+              title: "Translate with Google",
+              contexts: ["page", "link"],
+              documentUrlPatterns: ["*://*/*"],
+            },
+            () => chrome.runtime.lastError
+          );
+        }
       }
-      if (prefs['google-page']) {
-        chrome.contextMenus.create({
-          id: 'open-google',
-          title: 'Translate with Google',
-          contexts: ['page', 'link'],
-          documentUrlPatterns: ['*://*/*']
-        }, () => chrome.runtime.lastError);
-      }
-      if (prefs['bing-page']) {
-        chrome.contextMenus.create({
-          id: 'open-bing',
-          title: 'Translate with Bing',
-          contexts: ['page', 'link'],
-          documentUrlPatterns: ['*://*/*']
-        }, () => chrome.runtime.lastError);
-      }
-    });
+    );
   };
   chrome.runtime.onInstalled.addListener(onStartup);
   chrome.runtime.onStartup.addListener(onStartup);
 }
 chrome.contextMenus.onClicked.addListener(onClicked);
 
-chrome.action.onClicked.addListener(tab => chrome.storage.local.get({
-  'default-action': 'open-google'
-}, prefs => onClicked({
-  menuItemId: prefs['default-action'],
-  pageUrl: tab.url
-}, tab)));
-
-/* FAQs & Feedback */
-{
-  const {management, runtime: {onInstalled, setUninstallURL, getManifest}, storage, tabs} = chrome;
-  if (navigator.webdriver !== true) {
-    const page = getManifest().homepage_url;
-    const {name, version} = getManifest();
-    onInstalled.addListener(({reason, previousVersion}) => {
-      management.getSelf(({installType}) => installType === 'normal' && storage.local.get({
-        'faqs': true,
-        'last-update': 0
-      }, prefs => {
-        if (reason === 'install' || (prefs.faqs && reason === 'update')) {
-          const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
-          if (doUpdate && previousVersion !== version) {
-            tabs.create({
-              url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
-              active: reason === 'install'
-            });
-            storage.local.set({'last-update': Date.now()});
-          }
-        }
-      }));
-    });
-    setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
-  }
-}
+chrome.action.onClicked.addListener((tab) =>
+  chrome.storage.local.get(
+    {
+      "default-action": "open-google",
+    },
+    (prefs) =>
+      onClicked(
+        {
+          menuItemId: prefs["default-action"],
+          pageUrl: tab.url,
+        },
+        tab
+      )
+  )
+);
